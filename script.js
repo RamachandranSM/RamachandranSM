fa5c40e8fb24230e229854bd2d7ed4bb4ef10bde
RamachandranSM <ramachandrans212004@gmail.com>
2024-11-25 18:26:33 +0530

    improve blogs style

--- script.js
+++ script.js
@@ -8,24 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-
-//cards 
-const carousel = document.querySelector('.carousel');
-const prevButton = document.querySelector('.carousel-button.prev');
-const nextButton = document.querySelector('.carousel-button.next');
-
-let offset = 0;
-const cardWidth = 300; // Adjust based on card width
-const gap = 20; // Adjust based on gap
-
-prevButton.addEventListener('click', () => {
-    offset = Math.min(offset + (cardWidth + gap), 0);
-    carousel.style.transform = `translateX(${offset}px)`;
-});
-
-nextButton.addEventListener('click', () => {
-    const maxOffset = -(carousel.scrollWidth - carousel.offsetWidth);
-    offset = Math.max(offset - (cardWidth + gap), maxOffset);
-    carousel.style.transform = `translateX(${offset}px)`;
-});
